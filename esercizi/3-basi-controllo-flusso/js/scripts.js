2dd9268542a63927b569fa1d792b77024b11d6fd
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2024-10-22 16:48:10 +0200

    esercizi

---
+++ esercizi/3-basi-controllo-flusso/js/scripts.js
@@ -0,0 +1,34 @@
+/* ------------------------------- Esercizio 1 ------------------------------ */
+/*
+    Scrivere un programmino che dica se un numero è pari o dispari.
+*/
+
+/* ------------------------------- Esercizio 2 ------------------------------ */
+/*
+    Scrivi una catena di if-else in modo da soddisfare le seguanti consizioni per la scelta della taglia
+
+    num >= 20 corrisponde a "XL"
+    num < 20 corrisponde a "L"
+    num < 15 corrisponde a "M"
+    num < 10 corrisponde a "S"
+    num < 5 corrisponde a "XS"
+*/
+
+/* ------------------------------- Esercizio 3 ------------------------------ */
+/*
+    Dati 3 numeri, determina se un triangolo è equilatero, isoscele o scaleno.
+    Stampa in console:
+    "Il triangolo è equilatero" se tutti i lati sono uguali
+    "Il triangolo è isoscele" se almeno due lati sono uguali
+    "Il triangolo è scaleno" se tutti i lati sono diversi
+*/
+
+/* ------------------------------- Esercizio 4 ------------------------------ */
+/*
+    Scrivere uno switch statement che stabilisca in che categoria ricade il valore numerico dato:
+
+    da 1 a 3 = "Low"
+    da 4 a 6 = "Mid"
+    da 7 a 9 = "High"
+    > di 9 = "Il numero è maggiore di 9"
+*/
